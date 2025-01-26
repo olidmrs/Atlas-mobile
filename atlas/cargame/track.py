e2b2514b2d11b6bd9bbf2e27add23be4b73e1e00
liav-bolyasni <liav.bolyasni@gmail.com>
2025-01-26 01:27:29 -0500

    mastered the for loops

--- atlas/cargame/track.py
+++ atlas/cargame/track.py
@@ -8,10 +8,26 @@ class Track:
         self.pixels = pixels
     
     def get_start_position(self) -> Vector:
+        """
+        Looks through the pixels and finds the pixels which are rgb(255, 0, 0) and finds the start point associated with them
+        Returns the (x,y) position as a vector.
+        """
+        # self.pixels[0][0] -> (r, g, b): (int, int, int)
         pass
 
     def get_state(self) -> np.ndarray:
-        pass
+        """
+        Returns the pixels array
+        """
+        return self.pixels
     
-    def get_end_position(self) -> Vector:
-        pass
+    def get_end_positions(self) -> list[Vector]:
+        """
+        Looks through the pixels and finds all pixels which are rgb(0, 255, 0) and returns their (x,y) indices as Vectors
+        """
+        pixel_list_i = []
+        pixel_list_j = []
+        #for i in range(0, len(self.pixels) + 1):
+            #for j in range(1, len(self.pixels) + 1):
+
+        print(self.pixels)
